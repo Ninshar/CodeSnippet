69bdcb2fce36dcdb227492bd85e240fa3d389152
linxinliang <990386189@qq.com>
2017-07-27 17:18:12 +0800

    提交
    提交

---
+++ dome/js/index.js
@@ -0,0 +1,14 @@
+(function($){ 
+    $.fn.extend({  
+        loadingShow: function(image) {  
+			var loadingDiv ='<div id="loading" style="width:100%;height:100%;position: fixed;left: 0;top: 0;z-index:1001;background: rgba(0, 0, 0, 0.5);">'
+                                +'<div style="position: absolute;left: 0;top: 0;right: 0;bottom: 0;width: 100px;height:100px;margin: auto;background:url('+image+') center no-repeat;background-size: cover;"></div>'
+                            +'</div>';
+            $('body').append(loadingDiv);  
+		},  
+        loadingHide: function() {  
+            $('#loading').remove();
+        }  
+    })
+})(jQuery);
+			
